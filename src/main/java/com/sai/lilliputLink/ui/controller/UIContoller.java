ab2581d14325e1461cacbc7bd18b0fd83b407869
Saikrishna Akkenapelly <saikrishna.akkenapelly@gmail.com>
2021-12-24 21:07:24 +0530

    removed /home made it direct on load

--- src/main/java/com/sai/lilliputLink/ui/controller/UIContoller.java
+++ src/main/java/com/sai/lilliputLink/ui/controller/UIContoller.java
@@ -2,7 +2,6 @@ package com.sai.lilliputLink.ui.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,7 +14,6 @@ public class UIContoller
 	@Autowired
 	UIService uiService;
 	
-	@GetMapping(path = "/home")
 	public ModelAndView showHome()
 	{
 		ModelAndView modelAndView = new ModelAndView();

93b77f8f2052a19bab9e2e0a07212d8749030192
Saikrishna Akkenapelly <saikrishna.akkenapelly@gmail.com>
2021-12-24 21:36:26 +0530

    added default path

--- src/main/java/com/sai/lilliputLink/ui/controller/UIContoller.java
+++ src/main/java/com/sai/lilliputLink/ui/controller/UIContoller.java
@@ -2,6 +2,7 @@ package com.sai.lilliputLink.ui.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,6 +15,7 @@ public class UIContoller
 	@Autowired
 	UIService uiService;
 	
+	@GetMapping(path = "/")
 	public ModelAndView showHome()
 	{
 		ModelAndView modelAndView = new ModelAndView();
